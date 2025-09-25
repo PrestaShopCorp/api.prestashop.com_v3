1d2a45e0900a43df38287bf8365983065755c9df
Renaud Langlois <renaud.langlois@prestashop.com>
2025-09-25 14:59:48 +0200

    clean

--- app/Console/Commands/CheckPrestaShopVersionRetroCompat.php
+++ app/Console/Commands/CheckPrestaShopVersionRetroCompat.php
@@ -20,6 +20,9 @@ class CheckPrestaShopVersionRetroCompat extends Command
      */
     protected $description = 'Check retro compatibility for check_version.php';
 
+    private $url1 = 'https://api.prestashop.com';
+    private $url2 = 'http://localhost:8001/api';
+
     /**
      * @param string $contents
      * @return string
@@ -37,46 +40,49 @@ class CheckPrestaShopVersionRetroCompat extends Command
             : trim($bodyContents);
     }
 
+    private function checkVersion(string $url): void
+    {
+        echo "Testing Call: " . $url . "\n\n";
+
+        if (!$stream = fopen($this->url1 . $url, 'r')) {
+            return;
+        }
+        $contents = stream_get_contents($stream);
+        $bodyContents1 = $this->getBodyContents($contents);
+
+        if (!$stream = fopen($this->url2 . $url, 'r')) {
+            return;
+        }
+        $contents = stream_get_contents($stream);
+        $bodyContents2 = $this->getBodyContents($contents);
+
+        if ($bodyContents1 != $bodyContents2) {
+            echo "-------------- No -------------\n";
+            die();
+        } else {
+            echo "-------------- Yes -------------\n";
+        }
+        echo "\n\n";
+    }
+
     /**
      * Execute the console command.
      */
     public function handle(): void
     {
-        $url1 = 'https://api.prestashop.com';
-        $url2 = 'http://localhost:8001/api';
         $fileName = resource_path() . '/txt/api_calls.txt';
         $fileContents = file_get_contents($fileName);
         $calls = explode("\n", $fileContents);
+
         foreach ($calls as $call) {
             preg_match("/GET (.*) HTTP/", $call, $matches);
-            if (isset($matches[1]) && substr($matches[1], 0, 26) == '/version/check_version.php') {
+            if (!isset($matches[1])) {
+                break;
+            }
+            if (str_starts_with($matches[1], '/version/check_version.php')) {
+                //$this->checkVersion($matches[1]);
+            } else {
                 echo "Testing Call: " . $matches[1] . "\n\n";
-
-                if ($stream = fopen($url1 . $matches[1], 'r')) {
-                    $contents = stream_get_contents($stream);
-                    $bodyContents1 = $this->getBodyContents($contents);
-                    //echo 'Size 1: ' . strlen($bodyContents1) . "\n";
-                    //echo $bodyContents1."\n";
-                } else {
-                    break;
-                }
-
-                if ($stream = fopen($url2 . $matches[1], 'r')) {
-                    $contents = stream_get_contents($stream);
-                    $bodyContents2 = $this->getBodyContents($contents);
-                    //echo 'Size 2: ' . strlen($bodyContents2) . "\n";
-                    //echo $bodyContents2."\n";
-                } else {
-                    break;
-                }
-
-                if ($bodyContents1 != $bodyContents2) {
-                    echo "-------------- No -------------\n";
-                    die();
-                } else {
-                    echo "-------------- Yes -------------\n";
-                }
-                echo "\n\n";
             }
         }
     }
